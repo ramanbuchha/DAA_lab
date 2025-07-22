d2d9b0195d537be606bd2b81328cc3fe3e53cda4
Raman Buchha <buchab8@gmail.com>
2025-07-23 01:29:59 +0530

    Add files via upload

---
+++ fibo_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    printf("Enter the number of elements to print in fibonacci\n");
+    int n;
+    scanf("%d",&n);
+    int a=0,b=1,c=0;
+    printf("0 ");
+    printf("1 ");
+    for(int i=0;i<n-2;i++){
+        c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    return 0;
+}
